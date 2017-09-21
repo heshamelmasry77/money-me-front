d64a0d6524e4cede2a736c2d356b7bb0ffcd29a8
heshamelmasry77 <heshamelmasry@outlook.com>
2017-09-21 23:34:12 +0200

    More edits

--- src/app/techs/index.js
+++ src/app/techs/index.js
@@ -2,7 +2,7 @@ var angular = require('angular');
 
 var tech = require('./tech');
 var techs = require('./techs');
-
+require('./weekly_payouts_styles.scss');
 var techsModule = 'techs';
 
 module.exports = techsModule;
