9ed28865e8a19ed3bf9cc450802ee89fb8d5fd83
heshamelmasry77 <heshamelmasry@outlook.com>
2017-10-03 11:43:31 +0200

    Adding date to header

--- src/app/shared/header.js
+++ src/app/shared/header.js
@@ -1,3 +1,10 @@
 module.exports = {
-  template: require('./header.html')
+  template: require('./header.html'),
+  controller: headerController
 };
+
+/** @ngInject */
+function headerController() {
+  var vm = this;
+  vm.date = new Date();
+}
