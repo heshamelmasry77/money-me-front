d792ecbdab258b519d67aaebaadb5a0ff8c9fd23
heshamelmasry77 <heshamelmasry@outlook.com>
2017-10-10 18:02:06 +0200

    Changes for meta tags and title for the website

--- src/routes.js
+++ src/routes.js
@@ -6,27 +6,37 @@ function routesConfig($stateProvider, $urlRouterProvider, $locationProvider, ngM
   $urlRouterProvider.otherwise('/');
   ngMetaProvider.useTitleSuffix(true);
 
-  ngMetaProvider.setDefaultTitleSuffix(' | Best Website on the Internet!');
+  ngMetaProvider.setDefaultTitleSuffix(' | Best BTC Mining and HYIP Investing Monitoring website');
 
-  ngMetaProvider.setDefaultTag('author', 'John Smith');
+  ngMetaProvider.setDefaultTag('author', 'Hesham El Masry');
   $stateProvider.state('app', {
     url: '/',
     component: 'app',
     data: {
       meta: {
-        title: 'Pet Vaccinations - All You Need To Know | PawSquad',
-        'og:title': 'All You Need To Know About Pet Vaccinations',
-        description: 'Useful information about Routine Vaccines and Boosters for dogs and cats,   including start vaccines for puppies and kittens.',
-        'og:description': 'Useful information about Routine Vaccines and Boosters for dogs and cats,   including start vaccines for puppies and kittens.'
+        title: '*** MoneyMe - Hyip Monitor - Weekly Report ***',
+        'og:title': '*** MoneyMe - Hyip Monitor - Weekly Report ***',
+        description: 'MoneyMe a smart and easy way to invest your money. Their solution is suitable for those who are new to the world of crypto currencies, as well as for cryptocurrency experts and large-scale investors.',
+        'og:description': 'MoneyMe a smart and easy way to invest your money. Their solution is suitable for those who are new to the world of crypto currencies, as well as for cryptocurrency experts and large-scale investors.'
       }
     }
   }).state('topBitcoinMining', {
     url: '/top-bitcoin-mining',
     component: 'fountainTopBitcoinMining',
-    title: 'MoneyMe - Top Bitcoin Mining'
+    meta: {
+      title: 'MoneyMe - Top Bitcoin Mining',
+      'og:title': 'MoneyMe - Top Bitcoin Mining',
+      description: 'TOP BITCOIN MINING Programs Description Status Genesis-mining.com ⇒ Genes-mining.com Website ⇒ Genesis-mining.com Review Genesis-mining.com offers you a smart and easy way to invest your money. Their solution is suitable for those who are new to the world of crypto currencies, as well as for cryptocurrency experts and large-scale investors.',
+      'og:description': 'TOP BITCOIN MINING Programs Description Status Genesis-mining.com ⇒ Genes-mining.com Website ⇒ Genesis-mining.com Review Genesis-mining.com offers you a smart and easy way to invest your money. Their solution is suitable for those who are new to the world of crypto currencies, as well as for cryptocurrency experts and large-scale investors.'
+    }
   }).state('topPayingHyip', {
     url: '/top-paying-hyip',
     component: 'fountainTopPayingHyip',
-    title: 'MoneyMe - TOP Paying HYIP'
+    meta: {
+      title: 'MoneyMe - TOP Paying HYIP',
+      'og:title': 'MoneyMe - TOP Paying HYIP',
+      description: 'HYIPs are the ones that we invested and monitored. The premium hyip ranking is based on our experience and for your reference only.',
+      'og:description': 'HYIPs are the ones that we invested and monitored. The premium hyip ranking is based on our experience and for your reference only.'
+    }
   });
 }
